61dde9b9976d0ab0932f68ec19763bf981da68c6
Mohebbi <Mohebbi@ARIYANSOFT>
2017-10-13 01:19:04 +0330

    Add project files.

---
+++ PasswordGenerator/RandomNumber.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PasswordGenerator
+{
+    /// <summary>
+    /// By: Ahmadreza mohebbi
+    /// Project Manager at AriyanSoft
+    /// </summary>
+    public class RandomNumber
+    {
+        public static int GenerateNumber(int min, int max)
+        {
+            var rand = new Random();
+            return rand.Next(min, max);
+        }
+    }
+}
---
+++ PasswordGenerator/RandomPassword.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PasswordGenerator
+{
+    /// <summary>
+    /// By: Ahmadreza mohebbi
+    /// Project Manager at AriyanSoft
+    /// </summary>
+    public class RandomPassword
+    {
+        public static string RandomString(int size, bool lowerCase)
+        {
+            var builder = new StringBuilder();
+            var random = new Random();
+            char ch;
+            for (int i = 0; i < size; i++)
+            {
+                //Generate the number and convert it to a character
+                ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65)));
+                builder.Append(ch);
+            }
+            if (lowerCase)
+                return builder.ToString().ToLower();
+
+            return builder.ToString();
+        }
+    }
+}
